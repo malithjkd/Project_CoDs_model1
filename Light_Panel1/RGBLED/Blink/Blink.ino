6b4177bc666bde56d5cf0ba4c97f8648e5d7f2c7
malithjkd <malithjayawardhana@gmail.com>
2020-09-24 00:55:20 +0530

    bug fixied system panel running fine

--- Light_Panel1/RGBLED/Blink/Blink.ino
+++ Light_Panel1/RGBLED/Blink/Blink.ino
@@ -18,107 +18,128 @@ void setup()
   FastLED.addLeds<WS2812B, 3, RGB>(leds[1], NUM_LEDS);  // GRB ordering is typical
 }
 
-void loop() { 
-
-  
-  for(i=0;i<20;i++)
-  {
-    leds[0][i] = CRGB(10,99,97);  // Green,RED,BLUE
-    FastLED.show();
-    delay(50);
-    leds[0][i-1] = CRGB(0,0,0);  // Green,RED,BLUE
-    FastLED.show();
-    delay(10);  
-  }
+void loop() {  
+    for(i=0;i<20;i++)
+    {
+        leds[0][i] = CRGB(10,99,97);  // Green,RED,BLUE
+        FastLED.show();
+        delay(50);
+        leds[0][i-1] = CRGB(0,0,0);  // Green,RED,BLUE
+        FastLED.show();
+        delay(10);  
+    }
     leds[0][19] = CRGB(0,0,0);  // Green,RED,BLUE
     FastLED.show();
     delay(10);  
     delay(1500);
 
-  //process 7
+    //process 7
 
-  for(j=0;j<6;j++){
-    for(i=20;i<50;i++)
-    {
-      k=i;
-      leds[0][k] = CRGB(10,10,100);
-      FastLED.show();
-      delay(15);
-      l = i+7;
-      if(l<50)
-      {
-        k=l;
-      }else
-      {
-        k=l-30;  
-      }
-      leds[0][k] = CRGB(10,10,100);
-      FastLED.show();
-      delay(15);
-      l = i+14;
-      if(l<50)
-      {
-        k=l;
-      }else
-      {
-        k=l-30;  
-      }
-      leds[0][k] = CRGB(10,10,100);
-      FastLED.show();
-      delay(15);
-      l = i+22;
-      if(l<50)
-      {
-        k=l;
-      }else
-      {
-        k=l-30;  
-      }
-      leds[0][k] = CRGB(10,10,100);
-      FastLED.show();
-      delay(15);
-     
+    for(j=0;j<4;j++){
+        for(i=20;i<49;i++)
+        {
+            k=i;
+            leds[0][k] = CRGB(10,10,100);
+            FastLED.show();
+            delay(15);
+
+            l = i+7;
+            if(l<49)
+            {
+                k=l;
+            }else
+            {
+                k=l-29;  
+            }
+            
+            leds[0][k] = CRGB(10,10,100);
+            FastLED.show();
+            delay(15);
       
-      leds[0][i-1] = CRGB(0,0,0);
-      FastLED.show();
-      delay(15);
-      l = i+6;
-      if(l<50)
-      {
-        k=l;
-      }else
-      {
-        k=l-30;  
-      }
-      leds[0][k] = CRGB(0,0,0);
-      FastLED.show();
-      delay(15);
-      l = i+13;
-      if(l<50)
-      {
-        k=l;
-      }else
-      {
-        k=l-30;  
-      }
-      leds[0][k] = CRGB(0,0,0);
-      FastLED.show();
-      delay(15);
-      l = i+21;
-      if(l<50)
-      {
-        k=l;
-      }else
-      {
-        k=l-30;  
-      }
-      leds[0][k] = CRGB(10,10,100);
-      FastLED.show();
-      delay(15);
-
+            l = i+14;
+            if(l<49)
+            {
+                k=l;
+            }else
+            {
+                k=l-29;  
+            }
+            
+            leds[0][k] = CRGB(10,10,100);
+            FastLED.show();
+            delay(15);
+      
+            l = i+22;
+            if(l<49)
+            {
+                k=l;
+            }else
+            {
+                k=l-29;  
+            }
+            leds[0][k] = CRGB(10,10,100);
+            FastLED.show();
+            delay(15);
+      
+            //off
+            leds[0][i-1] = CRGB(0,0,0);
+            FastLED.show();
+            delay(15);
+      
+            l = i+6;
+            if(l<49)
+            {
+                k=l;
+            }else
+            {
+                k=l-29;  
+            }
+            leds[0][k] = CRGB(0,0,0);
+            FastLED.show();
+            delay(15);
+      
+            l = i+13;
+            if(l<49)
+            {
+                k=l;
+            }else
+            {
+                k=l-29;  
+            }
+            leds[0][k] = CRGB(0,0,0);
+            FastLED.show();
+            delay(15);
+            
+            l = i+21;
+            if(l<49)
+            {
+                k=l;
+            }else
+            {
+                k=l-29;  
+            }
+            leds[0][k] = CRGB(0,0,0);
+            FastLED.show();
+            delay(15);
+        }
+        leds[0][48] = CRGB(0,0,0);
+        FastLED.show();
+        delay(15);
     }
-  }
-  delay(1500);
+    leds[0][48] = CRGB(0,0,0);
+    FastLED.show();
+    delay(15);
+    leds[0][26] = CRGB(0,0,0);
+    FastLED.show();
+    delay(15);
+    leds[0][33] = CRGB(0,0,0);
+    FastLED.show();
+    delay(15);
+    leds[0][41] = CRGB(0,0,0);
+    FastLED.show();
+    delay(15);
+    
+    delay(1500);
 
   // process 10
   for(i=0;i<106;i++)
